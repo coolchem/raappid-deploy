68fc4be7df6ced444a900844806ecc3f08ca7208
varunreddy <varunreddy.nalagatla@davita.com>
2015-10-31 11:35:18 -0600

    initial checkin for raappid-deploy

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+
+var gulp = require("gulp");
+var ts = require('gulp-typescript');
+var del = require('del');
+var sourcemaps = require('gulp-sourcemaps');
+
+var tsProject = ts.createProject('tsconfig.json', {
+    typescript: require('typescript')
+});
+
+gulp.task('clean', function() {
+    del(['lib/**/*.js', 'index.js']).then(function (paths) {
+        console.log('Deleted files/folders:\n', paths.join('\n'));
+    });
+});
+
+gulp.task('build',['clean'], function() {
+    var tsResult = tsProject.src()
+        .pipe(sourcemaps.init()) // This means sourcemaps will be generated
+        .pipe(ts(tsProject));
+
+    return tsResult.js
+        .pipe(sourcemaps.write()) // Now the sourcemaps are added to the .js file
+        .pipe(gulp.dest('./'));
+});
---
+++ index.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+import gulp from "gulp";
+
